12d57f26f8eb4e1f3bf4364d8ed09c390aa2361a
Michał Poręba <michalporeba@gmail.com>
2024-02-17 18:55:00 +0000

    limiting repositories by explicitly specifying includes

--- get-data.js
+++ get-data.js
@@ -11,44 +11,84 @@ const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
 });
 
-console.log("\nStarting processing of github repos...");
+
+const checkRateLimit = async () => {
+    try {
+        const response = await octokit.rest.rateLimit.get();
+        
+        const remaining = response.data.rate.remaining;
+        const resetTime = new Date(response.data.rate.reset * 1000);
+
+        console.log(`\nRemaining API calls: ${remaining}`);
+        console.log(`Rate limit resets at: ${resetTime}`);
+
+    } catch (error) {
+        console.error("Error fetching rate limit information:", error);
+    }
+}
 
 const getDataForOrg = async (org) => {
-    return await octokit.rest.repos.listForOrg({
+    return await octokit.paginate(octokit.rest.repos.listForOrg, {
         org: org,
         type: "public",
-        per_page: 2,
+        per_page: 100,
+        headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+        },
     });
 };
 
 const getDataForUser = async (user) => {
-    return await octokit.rest.repos.listForUser({
+    return await octokit.paginate(octokit.rest.repos.listForUser, {
         username: user,
         type: "owner",
-        per_page: 2,
+        per_page: 100,
+        headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+        },
     });
 };
 
-const getData = (type) => {
-    if (type === "org") { return getDataForOrg; }
-    return getDataForUser;
+const getRepositories = (type, account) => {
+    try {
+        let getter = (type === "org") ? getDataForOrg : getDataForUser;
+        return getter(account);
+        myVar.people = myVar.people.filter( x => x.id === "192");
+    } catch (error) {
+        console.error(`Error fetching repositories for ${type} ${account}!`, error);
+    }
+    return [];
 }
 
-const processAccount = async ({account: account, type: type}) => {
-    console.log(`\nProcessing ${account}`);
+const processAccount = async ({name, type, include}) => {
+    console.log(`\nProcessing ${name}`);
     sleep(50);
 
-    let data = await getData(type)(account);
+    let repositories = await getRepositories(type, name);
+    let filteredRepositories = repositories.filter(r => !include || include.includes(r.name));
+    console.log(`${name} has ${filteredRepositories.length} repositories.`);
 
-    console.log(data);
+    //console.log(repositories);
 };
+
+console.log("\nStarting processing of github repos...");
 
 await (async () => {
     let accounts = [
-        { account: "uktrade", type: "org" },
-        { account: "michalporeba", type: "user" }];
+        { name: "uktrade", type: "org",
+            include: [
+                "data-workspace", "pg-bulk-ingest", "stream-zip", "stream-unzip",
+                "mbtiles-s3-server", "cypress-image-diff", "mobius3", "data-flow"
+            ]
+        },
+        { name: "michalporeba", type: "user",
+            include: [
+                "odis", "alps-py", "x-gov-repos", "static-data-publishing"
+            ]
+        }];
 
     await Promise.each(accounts, processAccount);
 })();
 
 console.log("\nDone!");
+await checkRateLimit();
